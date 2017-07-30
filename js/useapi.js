87c050a9e1b7a639c9d30a57a085747d41aa1dd4
Cuong Huynh <cuong.htan27@gmail.com>
2017-07-30 12:23:20 +0700

    Fix remote

--- js/useapi.js
+++ js/useapi.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-    $("button").click(function(){
-        $.getJSON('../skyscanner.json', function(data){
-          console.log(data);
-        });
-=======
 var sendRequest = function(){
 var FlightRequest = {
     "request": {
@@ -45,9 +38,54 @@ var FlightRequest = {
       error: function(){
        alert("Access to Google QPX Failed.");
      }
->>>>>>> origin/master
     });
-}
+};
+
+$(document).ready(function(){
+    $("button").click(function(){sendRequest();});
+});
+var sendRequest = function(){
+var FlightRequest = {
+    "request": {
+      "slice": [
+        {
+          "origin": "NRT",
+          "destination": "ICN",
+          "date": "2017-07-28"
+        }
+      ],
+      "passengers": {
+        "adultCount": 1,
+        "infantInLapCount": 0,
+        "infantInSeatCount": 0,
+        "childCount": 0,
+        "seniorCount": 0
+      },
+      "solutions": 20,
+      "refundable": false
+    }
+};
+
+    $.ajax({
+     type: "POST",
+     url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyA-Z_OABlCvKJj9Y7VWMNee6TaHQFbXFyI",
+     contentType: 'application/json',
+     dataType: 'json',
+     data: JSON.stringify(FlightRequest),
+     success: function (data) {
+
+      var myJSON = JSON.stringify(data);
+
+      console.log(myJSON);
+
+      $("#show").html(myJSON);
+
+    },
+      error: function(){
+       alert("Access to Google QPX Failed.");
+     }
+    });
+};
 
 $(document).ready(function(){
     $("button").click(function(){sendRequest();});
